71ee88e1a7d97906f2393c4689115e639e851cd2
Shubham Prakash <56962038+shubhamprkash@users.noreply.github.com>
2020-08-03 11:16:43 +0530

    Update and rename Practicle_(Healthcare)_Ques_2.sol to Practicle_2_Healthcare_Ques.sol

--- EXAM/Practicle_2_Healthcare_Ques.sol
+++ EXAM/Practicle_2_Healthcare_Ques.sol
@@ -1,7 +1,7 @@
-Healthcare question
-@ 0xEB18614CC9adCa1b55862C0A6E7019fa3921196C
-In https://ropsten.etherscan.io/address/0xeb18614cc9adca1b55862c0a6e7019fa3921196c
-
+/*  Healthcare question
+    @ 0xEB18614CC9adCa1b55862C0A6E7019fa3921196C
+    In https://ropsten.etherscan.io/address/0xeb18614cc9adca1b55862c0a6e7019fa3921196c
+*/
 pragma solidity ^0.4.17 < 0.6.12;
 
 contract Healthcare{
